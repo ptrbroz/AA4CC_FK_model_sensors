555e91a804b7f213442f68633fe86beec3ddac5b
ptrbroz <10211738+ptrbroz@users.noreply.github.com>
2022-01-27 11:20:39 +0100

    update python comm demo

--- FK_quadrature_decoder/python/CommDemo.py
+++ FK_quadrature_decoder/python/CommDemo.py
@@ -27,9 +27,13 @@ def arrayToInteger(arr):
 #-----------------------------------------
 #----------fpga comms functions-----------
 
-def initFpga(encoderVector, resolution, reset, miliseconds):
+def initFpga(encoderVector, resolution, revResolution, reset, miliseconds):
     bytesToSend = []
-    bytesToSend.append(b'\x01') #initialization command id
+    revResulotionNibble = (revResolution  << 4)
+    configIdNibble = 1
+    configIdByte = revResulotionNibble | configIdNibble;
+    configIdByte = configIdByte.to_bytes(1, 'big')  
+    bytesToSend.append(configIdByte) #initialization command id
     #first four data bytes contain first 32 bits of encoder vector
     for i in range(4):
         subVector = encoderVector[i*8:(i+1)*8]
@@ -62,11 +66,12 @@ def parseFpgaReply(databytes):
     
 
 #initialise fpga
-vector = [0]*1 + [0]*9 + [0]*15 + [1]*1 + [0]*9 #35 bit vector. 1 enables reading corresponding encoder, 0 disables it.
+vector = [1]*1 + [1]*9 + [0]*15 + [1]*1 + [1]*9 #35 bit vector. 1 enables reading corresponding encoder, 0 disables it.
 resolution      = 12              #1 to 13. Position resolution in bits. While settings 14 and 15 are also supported, don't use them unless you have a good reason to do so - see user manual (maximum resolution used internally is 13 bits, 14 and 15 just multiplies the output)
+revResolution   = 0             #0 to 8. Bit length of revolution counter representation. When 0, revolution counters are not sent.
 performReset    = 1               #1 or 0. When 1, positions will be reset; when 0, positions will be kept as they are
 waitTimeMs      = 10             #0 to 255. Minimum time between starts of messages in milliseconds. You might need to experiment with this one
-initFpga(vector, resolution, performReset, waitTimeMs)
+initFpga(vector, resolution, revResolution, performReset, waitTimeMs)
 
 #try to catch fpga reply - it should start with a header of 0xfffff0 and be followed by 7 data bytes
 while 1:
@@ -129,10 +134,15 @@ while(1):
     encoderCount = arrayToInteger(headerBitArray[6:12]) #first 6 bits of byte 2 don't carry data either
     resolution   = arrayToInteger(headerBitArray[12:16])
     
-    expectedDataBits = encoderCount*(resolution + 1) #+1 because of separator bits at start of every position
+    expectedDataBits = encoderCount*(resolution + 1) #+1 because of separator bits at start of every position   
+    
+    #add expected revolution counter bits
+    if revResolution != 0:
+        expectedDataBits = expectedDataBits + 4 #first four bits are secondary header
+        expectedDataBits = expectedDataBits + encoderCount*(revResolution+1) #each revolution counter is preceded by a zero separation bit
+    
     expectedBytes = math.ceil(expectedDataBits/8)    #don't forget to round up
 
-    
     dataBytes = []
     for i in range(expectedBytes):
         dataBytes.append(port.read())
@@ -143,10 +153,22 @@ while(1):
         bitList.extend(byteToArray(byte))
 
     x = resolution + 1
+    y = revResolution + 1
+
+    receivedRevRes = 0
+
+    if revResolution != 0:
+        receivedRevRes = arrayToInteger(bitList[encoderCount*x:encoderCount*x+4])
+
     positionsList = []
+    revolutionsList = []
+    revDataStart = encoderCount*x+4
 
     for i in range(encoderCount):
         positionsList.append(arrayToInteger(bitList[(i*x)+1:(i+1)*x])) #start at i*x+1 to skip separator bit
+        if revResolution != 0:
+            revolutionsList.append(arrayToInteger(bitList[revDataStart+(i*y)+1:revDataStart+(i+1)*y]))
+
     
     maxVal = (2**(resolution)) - 1
 
@@ -155,9 +177,15 @@ while(1):
         flicker = "\\"
     else:
         flicker = "/"
-    print(f"[{flicker}] I'm reading {encoderCount} encoders running at resolution of {resolution} bits.                       ")   
-    for i, position in enumerate(positionsList):
-        print(f"Encoder index {(i+1):02d}'s position: [{position:04d}/{maxVal}] ≈ {(position*360/maxVal):5.2f}°")
+        
+    if revResolution == 0:
+        print(f"[{flicker}] I'm reading {encoderCount} encoders running at resolution of {resolution} bits.     ")
+        for i, position in enumerate(positionsList):
+            print(f"Encoder index {(i+1):02d}'s position: [{position:04d}/{maxVal}] ≈ {(position*360/maxVal):5.2f}°")
+    else:
+        print(f"[{flicker}] I'm reading {encoderCount} encoders running at resolution of {resolution} bits and revRes of {receivedRevRes}   ")
+        for i, position in enumerate(positionsList):
+            print(f"Encoder index {(i+1):02d}'s position: [{position:04d}/{maxVal}], revs [{revolutionsList[i]}]")
 
     #carriage return / go up a line control codes to redraw printed data. Tested on Windows Terminal on windows 10. (available: https://github.com/microsoft/terminal)
     #does NOT work in Idle nor in the default windows cmd!!! 
